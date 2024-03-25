8e2fc3f38cb7610c4904da1c83b05d382ae6abd9
Alexander Fu <97246649+a22fu@users.noreply.github.com>
2024-03-24 22:49:41 -0400

    skeleton changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,26 @@
-import React from 'react';
-import {Header, About, Lilypad, Mainnav, Experience, Projects, Contacts} from './components';
+import React from "react";
+import {
+  Header,
+  About,
+  Lilypad,
+  Mainnav,
+  Experience,
+  Projects,
+  Contacts,
+} from "./components";
 
-import './styles/main.css';
+import "./styles/main.css";
 
 const App = () => {
   return (
-    
-    <div className='w-[calc(100vw-(100vw-100%))]'>
-      <Mainnav/>
-      <Header/>
-      <About/>
-      <Experience/>
-      <Projects/>
-      <Contacts/>
+    <div className="w-[calc(100vw-(100vw-100%))]">
+      <Mainnav />
+      <Header />
+      <About />
+      <Experience />
+      <Projects />
+      <Contacts />
     </div>
-    
   );
 };
 
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,12 +1,11 @@
-import React from 'react';
-import styles from './Header.module.css';
-import strings from '../../constants/strings.js';
+import React from "react";
+import styles from "./Header.module.css";
+import strings from "../../constants/strings.js";
 
 const Header = () => {
   return (
-    <div id = "header" className = {styles.screenFiller}>
-        <h1 className ={styles.title}>{strings.header.titleText}</h1>
-        <h2 className = {styles.subtitle}>{strings.header.subtitleText}</h2>
+    <div id="header" className={styles.screenFiller}>
+      <h1 className={styles.title}>{strings.header.titleText}</h1>
     </div>
   );
 };
